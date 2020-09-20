e800a185992d44187a5b4528b116d6e1c1ae046c
Carol Schiro <carol.schiro@yahoo.com>
2020-09-19 22:39:40 -0500

    final files

--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,35 @@
 // from data.js
 var tableData = data;
-var tbody = d3.select("tbody");
 
-tableData.forEach((buildTable) => {
-//  d3.event.preventDefault();
-  var row = tbody.append("tr");
-  Object.entries(buildTable).forEach(([key, value]) => {
-    var cell = row.append("td");
-    cell.text(value)
-});
+function tableBuild() {
+  var table = d3.select("tbody");
+  data.forEach((data) => {
+    var row = table.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+  });
+};
 
+tableBuild(tableData);
 
 var button = d3.select("#filter-btn");
 button.on("click", filter);
 
 function filter() {
     d3.event.preventDefault();
-   var inputData = d3.select("#datetime");
-   var value = inputData.property("value");
-   var data = tableData.filter(i => i.datetime === value);
-   table.html("");
+    var inputData = d3.select("#datetime");
+    var value = inputData.property("value");
+    var data = tableData.filter(j => j.datetime === value);
+    var table = d3.select("tbody");
+    table.html("");
 
-   tableData.forEach((data) => {
-      var row = tbody.append("tr");
-      Object.entries(data).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
+    data.forEach((data) => {
+        var row = table.append("tr");
+        Object.entries(data).forEach(([key, value]) => {
+            var cell1 = row.append("td");
+            cell1.text(value);
+        });
       });
-    });
-  };
-});
+};
